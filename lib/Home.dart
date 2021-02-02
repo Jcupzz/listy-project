4fb03eacb60df8c166d2936e345a47b37a25a9f1
Jcupzz <jcupzz255@gmail.com>
2021-02-02 20:40:12 +0530

    signin screen success

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'AuthenticationServices/AuthenticationService.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,6 +12,13 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true,title: Text("Listy"),),
+      body: FlatButton(child:Text("SignOut"),onPressed: () async{
+        dynamic isLoggedOut = await context.read<AuthenticationService>().signOut();
+        if(isLoggedOut.toString()=="Signed out")
+          {
+            Navigator.pushReplacementNamed(context, "/Register");
+          }
+      },),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:listy/Register.dart';
+import 'package:provider/provider.dart';
 
+import 'AuthenticationServices/AuthenticationService.dart';
 import 'Home.dart';
 import 'Login.dart';
+import 'Register.dart';
 import 'SplashScreen.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp(
-    initialRoute: "/SplashScreen",
-    home: Register(),
-    routes: {
-      '/SplashScreen':(context)=>SplashScreen(),
-      '/Login': (context) => Login(),
-      '/Register': (context) => Register(),
-      '/Home':(context)=>Home(),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [Provider<AuthenticationService>(create: (_)=>AuthenticationService(FirebaseAuth.instance)),
+        StreamProvider(create: (context)=>context.read<AuthenticationService>().authStateChanges,)
+        ],
+        child: MaterialApp(
+            home: AuthenticationWrapper(),
+            routes: {
+              '/SplashScreen': (context) => SplashScreen(),
+              '/Login': (context) => Login(),
+              '/Register': (context) => Register(),
+              '/Home': (context) => Home(),
+            }));
+  }
+}
+//
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    final firebaseUser = context.watch<User>();
+    print(firebaseUser);
+    if (firebaseUser == null) {
+    return Register();
+
     }
-  ));
+    else{
+      return Home();
+    }
+  }
 }
